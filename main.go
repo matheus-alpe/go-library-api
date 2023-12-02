07d57b9d90da9559de4d0203ef67810cc1fb43c5
Matt <matttalves@gmail.com>
2023-12-02 16:03:23 -0300

    init

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to Library API",
+		})
+	})
+
+	r.Run("localhost:3232")
+}
